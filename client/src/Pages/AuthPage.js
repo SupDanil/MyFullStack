c822f9caec5d39f2e9c98d2fc54ef25bf44787b5
Danil Kondratov <com.kondratov@gmail.com>
2021-10-17 15:26:43 +0300

    Добавил роут

---
+++ client/src/Pages/AuthPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const AuthPage = () =>{
+    return (
+        <div>
+            <h1>AUTH</h1>
+        </div>
+    )
+}
---
+++ client/src/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+import {LinksPage} from "./Pages/LinksPage";
+
+export const useRoutes = isAuthenticated =>{
+    if(isAuthenticated){
+        return(
+            <Switch>
+                <Route path="/links" exact>
+                    <LinksPage />
+                </Route>
+            </Switch>
+        )
+    }
+
+    return (
+        <Switch>
+
+        </Switch>
+    )
+
+}
