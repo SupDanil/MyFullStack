7b7c5d58807e914e301dec4199de2b7411ba9662
Danil Kondratov <com.kondratov@gmail.com>
2021-10-17 14:37:40 +0300

    Подключил React

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -2,7 +2,10 @@ const {check, validationResult} = require('express-validator')
 const {Router} = require('express');
 const bcrypt = require('bcryptjs')
 const router = Router();
-const User = require('../Models/User')
+const User = require('../Models/User');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+
 
 router.post(
     '/register',
@@ -41,8 +44,50 @@ router.post(
     }
 })
 
-router.post('/login', async (req, res) => {
+router.post(
+    '/login',
+    [
+        check('email','Введите корректный EMAIL').normalizeEmail().isEmail(),
+        check('password','Введите пароль').exists(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
+            if (errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Не корректные данне при входе в систему"
+                })
+            }
+
+            const {email, password} = req.body
+
+            const user = await User.findOne({email})
+
+            if (!user){
+                return res.status(400).json({message:"Пользователь не найден"});
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if(!isMatch){
+                return res.status(400).json({message: "Неверный пароль"})
+            }
+
+
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            res.json({token, userId: user.id});
+
+
+        } catch (e) {
+            res.status(500).json({message: "Что то пошло не так, попробуйте снова"})
+        }
 })
 
 module.exports = router;
